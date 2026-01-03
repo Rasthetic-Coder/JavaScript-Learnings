78f74a242593bdd45ca0417fd12f3407417485ad
Rashmi Pandey <your.rash02@gmail.com>
2026-01-03 20:18:53 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use scrict"; //treat all JS code as newer version
+
+// alert(3+5)   //we are using nodejs, not browser
+
+console.log(3 
+    + 
+    4);       //code readibility should be high
+
+console.log("Rashmi");
+
+
+let name = "Rishi"
+let age = "21"
+let isLoggedIn = false
+
+// number => 2 to the power 34
+//bigInt
+//string => ""
+//boolean => true/false
+//null => stanalone value
+// undefined 
+//symbol => uniques
+
+
+
+
+//object
+
+
+
+
+console.log(typeof null);    //object
+console.log(typeof undefined);  //undefined
